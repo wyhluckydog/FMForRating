8fd8d6ea35c6e33b014bd5c61e9a2f4ddcdf1bc1
wyhluckydog <wyh1962692398@163.com>
2019-11-09 10:07:44 +0800

    使用pytorch完成测试，测试rmse值为1.4

--- simple_model.py
+++ simple_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch as pt
+from torch.autograd import Variable
 from sklearn.feature_extraction import DictVectorizer
 
 cols=['user','item','rating','timestamp']
@@ -46,101 +47,71 @@ print("y_test shape",y_test.shape)
 n,p=x_train.shape
 
 class FM_model(pt.nn.Module):
-    def __init__(self,n,p,k):
-        super(FM_model, self).__init__()
-        self.n=n   #测试用例的数目
-        self.p=p
-        self.k=k
-        self.linear=pt.nn.Linear(self.n,1,bias=True)
-        self.v=pt.rand((self.p,self.k),requires_grad=True)
-        self.w=pt.rand((self.p,1),requires_grad=True)
-        # self.v=pt.normal(0,0.01,(self.p,self.k))
-        # self.w=pt.normal(0,0.1,(self.p,1))
-        self.w0=pt.rand((self.n,1),requires_grad=True)
+    def __init__(self,p,k):
+        super(FM_model,self).__init__()
+        self.p=p    #feature num
+        self.k=k     #factor num
+        self.linear=pt.nn.Linear(self.p,1,bias=True)   #linear part
+        self.v=pt.nn.Parameter(pt.rand(self.k,self.p))  #interaction part
 
     def fm_layer(self,x):
-        # print(self.w0[1:10])
-        #FM算法的线性部分
-        linear_term=pt.mm(pt.tensor(x).float(),self.w)
-        # print("linear_term shape",linear_term.shape)
-        pair_interactions=pt.sum(
-            pt.sub(
-                pt.pow(
-                    pt.mm(pt.tensor(x).float(),self.v),
-                    2
-                ),
-                pt.mm(
-                    pt.pow(pt.tensor(x).float(),2),
-                    pt.pow(self.v,2)
-                )
-            ),
-            dim=1
-        )
-        # print("pt.pow(pt.mm(pt.tensor(x).float(),self.v))",pt.pow(pt.mm(pt.tensor(x).float(),self.v)[0:10,1],2))
-        # print("pt.mm(pt.pow(pt.tensor(x).float(),2),pt.pow(self.v,2))",pt.mm(pt.pow(pt.tensor(x).float(),2),pt.pow(self.v,2))[0:10,1])
-        # print("pair_interactions shape",pair_interactions.shape)
-        # print("pair_interactions",pair_interactions[0:10])
-        # print("w0 shape",self.w0.shape)
-        output1=pt.add(self.w0.transpose(1,0),linear_term.transpose(1,0))
-        # print("output1 shape",output1.shape)
-        output2=pt.ceil(pt.add(output1,0.5*pair_interactions)/3)
-        # output=self.w0+linear_term+0.5*pair_interactions
-        return output2
+        #linear part
+        linear_part=self.linear(pt.tensor(x).float())
+        #interaction part
+        inter_part1=pt.mm(pt.tensor(x).float(),self.v.t())
+        inter_part2=pt.mm(pt.pow(pt.tensor(x).float(),2),pt.pow(self.v,2).t())
+        pair_interactions=pt.sum(pt.sub(pt.pow(inter_part1,2),inter_part2),dim=1)
+        output=linear_part.transpose(1,0)+0.5*pair_interactions
+        return output
 
-    def forward(self,x):
+    def forward(self, x):
         output=self.fm_layer(x)
         return output
 
-k=40   #因子的数目
-fm=FM_model(n,p,k)
+k=10   #因子的数目
+fm=FM_model(p,k)
 fm
-# print(len(list(fm.parameters())))
+# print("paramaters len",len(list(fm.parameters())))
+# # print(list(fm.parameters()))
+# for name,param in fm.named_parameters():
+#     if param.requires_grad:
+#         print(name)
 
 #训练网络
 optimizer=pt.optim.SGD(fm.parameters(),lr=0.01)   #学习率为0.01
-# optimizer.zero_grad()   #如果不置零，Variable的梯度在每次backwrd的时候都会累加
-# x_train=pt.autograd.Variable(pt.tensor(x_train),requires_grad=True)
-# y_train=pt.autograd.Variable(pt.tensor(y_train),requires_grad=True)
 for i in range(20):
     optimizer.zero_grad()  # 如果不置零，Variable的梯度在每次backwrd的时候都会累加
     output=fm(x_train)
-    # 处理内存溢出的问题
-    # try:
-    #     output=fm(x_train)
-    # except RuntimeError as exception:
-    #     if "not enough memory" in str(exception):
-    #         print("WARING:out of memeory")
-    #         if hasattr(pt.cuda,'empty_cache'):
-    #             pt.cuda.empty_cache()
-    #             output = fm(x_train)
-    #     else:
-    #         raise exception
-    # output=pt.autograd.Variable(output,requires_grad=True)
     output=output.transpose(1,0)
-    # print("output shape",output.shape)
     #平方差
     loss_func= pt.nn.MSELoss()
-    # mse_loss=loss_func(output,pt.tensor((output.shape,1)))
     mse_loss=loss_func(output,pt.tensor(y_train).float())
     l2_regularization=pt.tensor(0).float()
     # print("l2_regularization type",l2_regularization.dtype)
-    #打印参数数量
-    # print(fm.parameters())
-    # print(len(list(fm.parameters())))
     #加入l2正则
     for param in fm.parameters():
         # print("param type",pt.norm(param,2).dtype)
         l2_regularization+=pt.norm(param,2)
-    # loss=mse_loss
-    # print(loss)
     loss=mse_loss+l2_regularization
     loss.backward()
     optimizer.step()   #进行更新
     # print(loss)
 
-print(loss)
-print(y_train[0:10],"  ",output[0:10])
-#精度函数
+# print("train_loss",loss)
+# print(y_train[0:10],"  ",output[0:10])
+
+#评价指标rmse
+def rmse(pred_rate,real_rate):
+    #使用均方根误差作为评价指标
+    loss_func=pt.nn.MSELoss()
+    mse_loss=loss_func(pred_rate,pt.tensor(real_rate).float())
+    rmse_loss=pt.sqrt(mse_loss)
+    return rmse_loss
+
 
 #测试网络
-
+pred=fm(x_test)
+pred=pred.transpose(1,0)
+rmse_loss=rmse(pred,y_test)
+print("test_loss",rmse_loss)
+print(y_test[0:5]," ",pred[0:5])
